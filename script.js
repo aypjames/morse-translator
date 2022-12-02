c564cd1091c0c9f785c114c597c76fa0d2998618
Ayush James <ayushpjames@gmail.com>
2022-12-03 07:15:39 +1100

    setup unit testing for logic functions

--- script.js
+++ script.js
@@ -1,102 +1,14 @@
-const morseAlphabet = {
-  A: ".-",
-  B: "-...",
-  C: "-.-.",
-  D: "-..",
-  E: ".",
-  F: "..-.",
-  G: "--.",
-  H: "....",
-  I: "..",
-  J: ".---",
-  K: "-.-",
-  L: ".-..",
-  M: "--",
-  N: "-.",
-  O: "---",
-  P: ".--.",
-  Q: "--.-",
-  R: ".-.",
-  S: "...",
-  T: "-",
-  U: "..-",
-  W: ".--",
-  X: "-..-",
-  Y: "-.--",
-  Z: "--..",
-  " ": "/",
-  0: "-----",
-  1: ".----",
-  2: "..---",
-  3: "...--",
-  4: "....-",
-  5: ".....",
-  6: "-....",
-  7: "--...",
-  8: "---..",
-  9: "----.",
-  ",": "--..--",
-  ".": ".-.-.-",
-  "?": "..--..",
-  ";": "-.-.-.",
-  ":": "---...",
-  "/": "-..-.",
-  "-": "-....-",
-  "(": "-.--.",
-  ")": "-.--.-",
-  "=": "-...-",
-  "+": ".-.-.",
-  "@": ".--.-.",
-};
+import {
+  checkNormOrMorse,
+  convertNormToMorse,
+  convertMorseToNorm,
+} from "./functionScripts.js";
 
-// NEEED TO HIGHLIGHT WHAT COULD NOT BE TRANSLATED! EG> *
+// NEED TO HIGHLIGHT WHAT COULD NOT BE TRANSLATED! EG the character  "*"
 
 const userInput = document.getElementById("userInput");
 const appOutput = document.getElementById("appOutput");
-
-const checkNormOrMorse = (string) => {
-  if (string[0] === "." || string[0] === "/" || string[0] === "-") {
-    return "Morse Code";
-  } else {
-    return "Normal Text";
-  }
-};
-
-const convertNormToMorse = (string) => {
-  // Converts the String to UpperCase
-  // Splits the string to individual characters in an array.
-  // Map through the strToArr array and For each character in the array, check value against the key from the moreAlphabet object and return the value.
-  // Join the array together to form a single string
-
-  const morseStrToUpperCase = string.toUpperCase();
-  const morseStrToArr = morseStrToUpperCase.split("");
-  const normToMorse = morseStrToArr.map((X) => morseAlphabet[X]);
-  const outPut = normToMorse.join(" ");
-
-  return outPut;
-};
-
-const convertMorseToNorm = (string) => {
-  // Splits the string to individual morse codes in an array.
-  // Map through the strToArr array and For each code in the array, find the key from the moreAlphabet object by checking against value and return the value.
-  // Join the array together to form a single string
-  // Convert to lowercase
-
-  const normStrToArr = string.split(" ");
-  const morseToNorm = normStrToArr.map((morseCodeValue) => {
-    const objKeysArr = Object.keys(morseAlphabet);
-    return objKeysArr.find((key) => morseAlphabet[key] === morseCodeValue);
-  });
-  const outPut = morseToNorm.join("");
-
-  return outPut.toLowerCase();
-};
-
-console.log(convertMorseToNorm(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")); // Should return hello world
-
-console.log(convertNormToMorse("hello world")); // Should return .... . .-.. .-.. --- / .-- --- .-. .-.. -..
-
-const langDetected = document.getElementById("langDetected");
+// Gathering Inputs and posting outputs.
 
 const domManipulation = () => {
   userInput.addEventListener("keyup", () => {
@@ -125,6 +37,10 @@ const domManipulation = () => {
 };
 
 domManipulation();
+
+console.log(convertMorseToNorm(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")); // Should return hello world
+
+console.log(convertNormToMorse("hello world")); // Should return .... . .-.. .-.. --- / .-- --- .-. .-.. -..
 
 //
 // How to handle numbers and stuff? throw error that currently this version only handles words?
--- script.test.js
+++ script.test.js
@@ -1,5 +1,47 @@
-// first confirm valid values are correct.
+import {
+  checkNormOrMorse,
+  convertNormToMorse,
+  convertMorseToNorm,
+} from "./functionScripts.js";
 
-// Values are not . - or / or space
+describe("Tests whether the input from the user is Normal Text, Morse Code or an invalid character", () => {
+  it("Should return 'Morse Code', if input text starts with either '.' or '-'", () => {
+    expect(checkNormOrMorse(".--.--")).toBe("Morse Code");
+    expect(checkNormOrMorse(".00029")).toBe("Morse Code");
+    expect(checkNormOrMorse("-12")).toBe("Morse Code");
+  });
 
-//
+  it("Should return 'Normal Text', if input text is every other character'", () => {
+    expect(checkNormOrMorse("hello world")).toBe("Normal Text");
+    expect(checkNormOrMorse("0900-/.")).toBe("Normal Text");
+    expect(checkNormOrMorse("@@@")).toBe("Normal Text");
+  });
+
+  it("Should throw an error for invalid characters", () => {
+    expect(checkNormOrMorse("*")).toBe("Error");
+    expect(checkNormOrMorse("~")).toBe("Error");
+    expect(checkNormOrMorse("}")).toBe("Error");
+  });
+});
+
+describe("Testing that a 'normal' text string is converted into morse code", () => {
+  it("Should return only '.', '/' or '-' characters and have spaces between each code snippet and '/' between each word", () => {
+    expect(convertNormToMorse("hello world")).toBe(
+      ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+    );
+    expect(convertNormToMorse("hello")).toBe(".... . .-.. .-.. ---");
+    expect(convertNormToMorse("h")).toBe("....");
+    expect(convertNormToMorse("@")).toBe(".--.-.");
+  });
+});
+
+describe("Testing that a morse code string is converted into 'normal' text", () => {
+  it("Should only return alphabetical characters[a-z] or numbers[0-9] and some special characters. Sentences should have spaces between words", () => {
+    expect(
+      convertMorseToNorm(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")
+    ).toBe("hello world");
+    expect(convertMorseToNorm(".... . .-.. .-.. ---")).toBe("hello");
+    expect(convertMorseToNorm("....")).toBe("h");
+    expect(convertMorseToNorm(".--.-.")).toBe("@");
+  });
+});
